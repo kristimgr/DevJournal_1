b57f8e8df6993c9d9ee083836fe880bf71fc3485
kristimgr <kristishreesmagar242@gmail.com>
2026-01-08 11:14:47 +0545

    Describe changes

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,20 +1,18 @@
-<<<<<<< HEAD
-﻿using DevJournal.Components.Pages;
+using DevJournal.Components.Pages;
 using DevJournal.Services;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace DevJournal
 {
 	public partial class App : Application
 	{
-		public App()
+		public App(IServiceProvider serviceProvider)
 		{
 			InitializeComponent();
 
-			// Get the service from MauiProgram
-			var journalService = MauiProgram.Services.GetRequiredService<IJournalService>();
+			var journalService = serviceProvider.GetRequiredService<IJournalService>();
 
-			// Create MainPage with the service
-			MainPage = new NavigationPage(new MainPage(journalService))
+			MainPage = new NavigationPage(new MainPage())
 			{
 				BarBackgroundColor = Color.FromArgb("#34495E"),
 				BarTextColor = Colors.White
@@ -22,19 +20,3 @@ namespace DevJournal
 		}
 	}
 }
-=======
-﻿namespace DevJournal;
-
-public partial class App : Application
-{
-	public App()
-	{
-		InitializeComponent();
-	}
-
-	protected override Window CreateWindow(IActivationState? activationState)
-	{
-		return new Window(new MainPage()) { Title = "DevJournal" };
-	}
-}
->>>>>>> fde1b6e0ea6b5f8641aab2f43c7d019b8b89d3d9
--- MauiProgram.cs
+++ MauiProgram.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.Extensions.Logging;
-<<<<<<< HEAD
 using DevJournal.Services;
 
 namespace DevJournal
@@ -33,30 +32,3 @@ namespace DevJournal
 		}
 	}
 }
-=======
-
-namespace DevJournal;
-
-public static class MauiProgram
-{
-	public static MauiApp CreateMauiApp()
-	{
-		var builder = MauiApp.CreateBuilder();
-		builder
-			.UseMauiApp<App>()
-			.ConfigureFonts(fonts =>
-			{
-				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-			});
-
-		builder.Services.AddMauiBlazorWebView();
-
-#if DEBUG
-		builder.Services.AddBlazorWebViewDeveloperTools();
-		builder.Logging.AddDebug();
-#endif
-
-		return builder.Build();
-	}
-}
->>>>>>> fde1b6e0ea6b5f8641aab2f43c7d019b8b89d3d9
